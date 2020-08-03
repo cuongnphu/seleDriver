5ba685eabcc8f7025d11d4a506a1e5d544f80566
cuongnguyenp <cuong.phu.nguyen128@gmail.com>
2020-08-03 11:50:23 +0700

    modify testng.xml

--- src/test/java/com/cuong/GosellTest.java
+++ src/test/java/com/cuong/GosellTest.java
@@ -3,19 +3,27 @@ package com.cuong;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 
-public class Gosell {
+public class GosellTest {
 
-    public static void main(String[] args) {
 
+    @Test
+    public void test(){
         WebDriverManager.chromedriver().setup();
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://admin.unisell.vn/login");
         webDriver.manage().window().maximize();
-
-//        webDriver.close();
-//        webDriver.quit();
-
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        webDriver.close();
+        webDriver.quit();
     }
+
+
+
 }
