161a53526d7f1bc89142305e66907f102fcbf1d5
cuongnguyenp <cuong.phu.nguyen128@gmail.com>
2020-08-04 07:15:58 +0700

    update

--- src/test/java/com/cuong/GosellTest.java
+++ src/test/java/com/cuong/GosellTest.java
@@ -3,6 +3,7 @@ package com.cuong;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 
@@ -13,7 +14,14 @@ public class GosellTest {
 //        WebDriverManager.chromedriver().setup();
 
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver");
-        WebDriver webDriver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        WebDriver webDriver = new ChromeDriver(options);
 
         webDriver.get("https://admin.unisell.vn/login");
         webDriver.manage().window().maximize();
