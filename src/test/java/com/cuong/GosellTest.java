5c9c52ff45930b1b4325fc449c5a905fd05a1256
cuong.nguyen@mediastep.com <cuong.nguyen@mediastep.com>
2020-08-06 15:00:59 +0700

    pom.xml

--- src/test/java/com/cuong/GosellTest.java
+++ src/test/java/com/cuong/GosellTest.java
@@ -26,15 +26,6 @@ public class GosellTest {
 
 //        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver");
 
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("start-maximized"); // open Browser in maximized mode
-//        options.addArguments("disable-infobars"); // disabling infobars
-//        options.addArguments("--disable-extensions"); // disabling extensions
-//        options.addArguments("--disable-gpu"); // applicable to windows os only
-//        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-//        options.addArguments("--no-sandbox"); // Bypass OS security model
-
-
 //        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
 
         WebDriver webDriver = new ChromeDriver();
