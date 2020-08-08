23ab157cb45f65d5579eb39c6f3227d4997a4616
cuong.nguyen@mediastep.com <cuong.nguyen@mediastep.com>
2020-08-08 16:54:19 +0700

    pom.xml

--- src/test/java/com/cuong/GosellTest.java
+++ src/test/java/com/cuong/GosellTest.java
@@ -3,16 +3,10 @@ package com.cuong;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-import java.util.Collections;
-import java.util.HashMap;
 
 
 public class GosellTest {
@@ -22,13 +16,18 @@ public class GosellTest {
 
          String  useName = "input#username";
          String pass = "input#password";
+
         WebDriverManager.chromedriver().setup();
 
 //        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver");
 
 //        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
 
-        WebDriver webDriver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        chromeOptions.addArguments("--headless");
+
+        WebDriver webDriver = new ChromeDriver(chromeOptions);
 
         webDriver.get("https://admin.unisell.vn/login");
         webDriver.manage().window().maximize();
