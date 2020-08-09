a7cea6869f756b1115ecb9500c10d6f23eb4e349
cuong.nguyen@mediastep.com <cuong.nguyen@mediastep.com>
2020-08-09 15:43:26 +0700

    pom.xml

--- src/test/java/com/cuong/GosellTest.java
+++ src/test/java/com/cuong/GosellTest.java
@@ -2,32 +2,27 @@ package com.cuong;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
-
 
 
 public class GosellTest {
 
     @Test
-    public void test(){
+    public void test() {
 
-         String  useName = "input#username";
-         String pass = "input#password";
+        String useName = "input#username";
+        String pass = "input#password";
 
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
 
 //        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver");
-
 //        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
 
-        ChromeOptions chromeOptions = new ChromeOptions();
 
-//        chromeOptions.addArguments("--headless");
-
-        ChromeDriver webDriver = new ChromeDriver(chromeOptions);
+        WebDriver webDriver = new FirefoxDriver();
 
         webDriver.get("https://admin.unisell.vn/login");
         webDriver.manage().window().maximize();
